71834b236922ec8e618cf8ff48bfe6182df74df8
Jose Antonio <josandluq@alum.us.es>
2015-11-17 17:57:47 +0100

    Incluye el fichero rectangulo.py
    Este fichero inlcluye la implementacion de rectangulo con su constructor y un metodo para calcular el area de este pero documentado

--- rectangulo.py
+++ rectangulo.py
@@ -3,11 +3,11 @@ class rectangulo:
 		self.x = x
 		self.y = y
 
-	def area(self):
-		return self.x * self.y
+	"""def area(self):
+		return self.x * self.y"""
 
 # ejemplo.py
 from rectangulo import rectangulo
 
 x = rectangulo(1, 2)
-print x.area()
+"""print x.area()"""
